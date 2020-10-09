71041d4a1d4cd678e5ca0714702dae6aa4361569
Pål Grønstad Solheim <paul.solheim@gmail.com>
2020-10-09 15:10:14 +0200

    Streetlights
    Lagt inn Streetlights.

--- World/ObjectSpawner.gd
+++ World/ObjectSpawner.gd
@@ -7,6 +7,7 @@ var number_of_parked_cars = 100
 var number_of_billboards = 75
 var number_of_traffic_cones = 40
 var number_of_hydrants = 50
+var number_of_streetlights = 50
 
 func generate_props(tile_list, size):
 	tiles = tile_list
@@ -15,6 +16,7 @@ func generate_props(tile_list, size):
 	place_billboards()
 	place_traffic_cones()
 	place_hydrants()
+	place_streetlights()
 
 func random_tiles(tile_count):
 	var tile_list = tiles
@@ -97,3 +99,20 @@ sync func spawn_hydrants(tile, hydrants_rotation):
 	hydrants.rotation_degrees.y = hydrants_rotation
 	add_child(hydrants, true)
 
+func place_streetlights():
+	var tile_list = random_tiles(number_of_streetlights)
+	for i in range(number_of_streetlights):
+		var tile = tile_list[0]
+		var tile_type = get_node("..").get_cell_item(tile.x, 0, tile.z)
+		var allowed_rotations = $ObjectRotLookup.lookup_rotation(tile_type)
+		if not allowed_rotations == null:
+			var tile_rotation = allowed_rotations[randi() % allowed_rotations.size() -1] * -1
+			tile.y = tile.y+0.5  # adjust for the height of the cars
+			rpc("spawn_streetlights", tile, tile_rotation)
+		tile_list.pop_front()
+
+sync func spawn_streetlights(tile, streetlights_rotation):
+	var streetlights = preload("res://Props/StreetLight/StreetLight.tscn").instance()
+	streetlights.translation = Vector3((tile.x * 20) +10, tile.y, (tile.z * 20) +10)
+	streetlights.rotation_degrees.y = streetlights_rotation
+	add_child(streetlights, true)
